f6d56814c848df1d060e386ad2da0b677043100e
ADVANCIO\DanielT <danielt@advancio.com>
2016-10-03 12:26:07 -0700

    Comment added, Second commint.
    testing commint

--- src/motor/IgnitionMotor.java
+++ src/motor/IgnitionMotor.java
@@ -3,7 +3,7 @@ package motor;
 import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
-
+//Glue code for feature files 
 @RunWith(Cucumber.class)
 @Cucumber.Options(features="features",glue={"pasos"})
 public class IgnitionMotor {
